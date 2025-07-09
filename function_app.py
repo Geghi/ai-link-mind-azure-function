8efcfb071e9e5b287bc2428ac1a8ade167d79b1f
Giacomo <mantovani.giacomo96@gmail.com>
2025-07-09 11:58:37 +0200

    fix: Remove extraneous character from sys.path append statement

--- function_app.py
+++ function_app.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 # Add the project root to the Python path to enable absolute imports from 'src'
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))ù
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
 # from src.services.scraper import get_internal_links, get_page_text_content, get_page_html_content
 from src.services.scraped_pages_service import insert_scraped_page, update_scraped_page_status
